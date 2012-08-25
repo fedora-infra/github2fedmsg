5aad8c4c0171a3ec71a23403acf00e07f8184732
Ralph Bean <rbean@redhat.com>
2012-08-25 09:09:37 -0400

    Store new users in the DB.

--- statatat/models.py
+++ statatat/models.py
@@ -2,27 +2,23 @@ from sqlalchemy import (
     Column,
     Integer,
     Text,
-    )
+)
 
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
-    )
+)
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
+Base.query = DBSession.query_property()
 
-class MyModel(Base):
-    __tablename__ = 'models'
+
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
+    username = Column(Text, unique=True)
--- statatat/views.py
+++ statatat/views.py
@@ -5,10 +5,7 @@ from sqlalchemy.exc import DBAPIError
 
 from moksha.api.widgets import get_moksha_socket
 
-from .models import (
-    DBSession,
-    MyModel,
-)
+import statatat.models as m
 
 
 def with_moksha_socket(func):
@@ -23,32 +20,22 @@ def with_moksha_socket(func):
 @view_config(route_name='home', renderer='index.mak')
 @with_moksha_socket
 def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name=='one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one':one, 'project':'statatat'}
+    return {}
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
 
-1.  You may need to run the "initialize_statatat_db" script
-    to initialize your database tables.  Check your virtual 
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
-
-@view_config(context='velruse.AuthenticationComplete', renderer='json')
+@view_config(context='velruse.AuthenticationComplete')
 def github_login_complete_view(request):
-    return {"access": "granted"}
+    username = request.context.profile['preferredUsername']
+
+    query = m.User.query.filter_by(username=username)
+    if query.count() == 0:
+        m.DBSession.add(m.User(username=username))
+
+    # TODO -- how not to hard code this location?
+    return HTTPFound(location="/")
+
 
 @view_config(context='velruse.AuthenticationDenied', renderer='json')
 def login_denied_view(request):
+    # TODO -- fancy flash and redirect
     return {'result': 'denied'}
