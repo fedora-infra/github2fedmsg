9ac1865c638b2c0597a57b7c9a3a6777669970ee
Ralph Bean <rbean@redhat.com>
2012-08-26 09:15:55 -0400

    Authentication++

--- statatat/__init__.py
+++ statatat/__init__.py
@@ -2,26 +2,38 @@ from pyramid.config import Configurator
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
 from pyramid.authentication import AuthTktAuthenticationPolicy
 #from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.security import authenticated_userid
 from sqlalchemy import engine_from_config
 
-from .models import DBSession
+import statatat.models
 
 # TODO -- replace this with pyramid_beaker
 crappy_session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
 authn_policy = AuthTktAuthenticationPolicy(secret='verysecret')
 #authz_policy = ACLAuthorizationPolicy()
 
+
+def get_user(request):
+    """ A utility property hanging on 'request' """
+    username = authenticated_userid(request)
+    query = statatat.models.User.query.filter_by(username=username)
+    if username and query.count() > 0:
+        return query.one()
+
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
+    statatat.models.DBSession.configure(bind=engine)
     config = Configurator(
         settings=settings,
         session_factory=crappy_session_factory,
         authentication_policy=authn_policy,
         #authorization_policy=authz_policy,
     )
+    # Make it so we can do "request.user" in templates.
+    config.set_request_property(get_user, 'user', reify=True)
 
     config.include('velruse.providers.github')
     config.add_github_login_from_settings()
@@ -31,4 +43,3 @@ def main(global_config, **settings):
     config.add_route('logout', '/logout')
     config.scan()
     return config.make_wsgi_app()
-
--- statatat/templates/index.mak
+++ statatat/templates/index.mak
@@ -1,11 +1,17 @@
 <html>
   <head></head>
   <body>
-    <form action="/login/github" method="post">
-      <input type="submit" value="Login with Github" />
-    </form>
-
-    Hai there.
+    % if request.user:
+      <span class="welcome">Welcome,
+        <span class="username">${request.user.username}</span>
+      </span>
+      <span class="logout"><a href="/logout">Logout</a></span>
+    % else:
+      <form action="/login/github" method="post">
+        <input type="submit" value="Login with Github" />
+      </form>
+      Hai there.
+    % endif
     ${moksha_socket.display() | n}
   </body>
 </html>
