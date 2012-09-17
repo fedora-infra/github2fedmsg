7f7edfd9703bdc1021878b08fd44f3f068011d33
Ralph Bean <rbean@redhat.com>
2012-09-17 00:50:05 -0400

    Forward posts from github on to the moksha hub.

--- statatat/views/__init__.py
+++ statatat/views/__init__.py
@@ -1,6 +1,9 @@
 from pyramid.view import view_config
 from pyramid.security import authenticated_userid
 
+import moksha.hub.hub
+import json
+
 
 @view_config(route_name='home', renderer='index.mak')
 def home(request):
@@ -8,11 +11,23 @@ def home(request):
     return {}
 
 
+
 @view_config(route_name='webhook', renderer='string')
 def webhook(request):
     """ Handle github webhook. """
-    import pprint
-    pprint.pprint(request.params)
+
+    if 'payload' in request.params:
+        payload = request.params['payload']
+        if isinstance(payload, basestring):
+            payload = json.loads(payload)
+
+        topic = "%s.%s" % (
+            payload['repository']['owner']['name'],
+            payload['repository']['name']
+        )
+        hub = moksha.hub.hub.MokshaHub(request.registry.settings)
+        hub.send_message(topic=topic, message=payload)
+
     return "OK"
 
 
