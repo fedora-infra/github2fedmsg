04cec4ab3cd81135880643e30cdbe39a87ac3141
Ralph Bean <rbean@redhat.com>
2013-02-24 12:12:23 -0500

    Remove more old statatat cruft from traversal.

--- pep8bot/traversal.py
+++ pep8bot/traversal.py
@@ -42,27 +42,6 @@ class UserApp(pep8bot.widgets.UserProfile):
     __name__ = None
     __parent__ = RootApp
 
-    @classmethod
-    def __getitem__(self, key):
-        if key == 'new':
-            return pep8bot.widgets.NewWidgetWidget(user=self.user)
-
-        # I dunno about this yet.. what is this app going to do?
-        raise NotImplementedError("The stuff below this needs thinking over..")
-        suffix = '.widget'
-        if key.endswith(suffix):
-            # Visiting /username/my_widget produces a user page detailing the
-            # widget, what options it has, displaying it...  Visiting
-            # /username/my_widget.widget produces the embeddable version.
-            chrome = False
-            key = key[:-len(suffix)]
-
-        for conf in self.user.widget_configurations:
-            if conf.name == key:
-                return pep8bot.widgets.make_widget(conf, chrome)
-
-        raise KeyError("No such widget %r" % key)
-
 
 class APISuccess(object):
     def __init__(self, data):
